227e510fdf09790f2204ccdd1efc25775ba01374
Scar 3 <kadansk8@gmail.com>
2024-02-10 10:27:36 -0500

    home scaling almost done

---
+++ src/components/About.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import Intro from "./Intro";
+import "../assets/stylesheets/about.scss";
+
+export default class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="about">
+        <div className="about-divider"></div>
+        <div className="about-content">
+          <span className="about-emoji">✏️</span>
+          <div className="about-text">
+            <span className="about-title">About</span>
+            <div className="about-description">
+              <span className="about-p">Hello, my name is </span>
+              <span className="about-p-bold">Scarlett</span>
+              <span className="about-p">
+                . I'm a nineteen-year-old developer from{" "}
+              </span>
+              <span className="about-p-bold">New York State</span>
+              <span className="about-p">
+                , deeply passionate about technology. I'm currently in my second
+                year of college, working towards earning a Bachelor's Degree in{" "}
+              </span>
+              <span className="about-p-bold">Computer Science</span>
+              <span className="about-p">
+                . From a young age, I've been fascinated by computers and their
+                impact on our daily lives, which is what led me to start my
+                journey in tech.
+              </span>
+              <br />
+              <br />
+              <span className="about-p-bold">
+                I have used and feel comfortable with the following
+                technologies:
+              </span>
+              <div className="about-p-list">
+                <span className="about-p-list-item">• Javascript</span>
+                <span className="about-p-list-item">• HTML + CSS</span>
+                <span className="about-p-list-item">• React</span>
+                <span className="about-p-list-item">• Python</span>
+                <span className="about-p-list-item">• C#</span>
+                <span className="about-p-list-item">• MySQL</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="about-divider"></div>
+      </div>
+    );
+  }
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,80 +1,18 @@
 import React, { Component } from "react";
-import Sparkle from "../assets/svgs/sparkle.svg";
+import Intro from "./Intro";
+import About from "./About";
 import "../assets/stylesheets/home.scss";
-import "../assets/stylesheets/glitch.scss";
-import "../assets/stylesheets/sparkle.scss";
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      typing: "student🎓",
-    };
-    this.time = 0;
-    this.prompt = 0;
-  }
-
-  titles = ["student🎓", "developer💻", "leader💡", "creator🎨"];
-
-  tick() {
-    this.time++;
-    if (this.time === 5) {
-      this.time = 0;
-      this.prompt++;
-      if (this.prompt === this.titles.length) {
-        this.prompt = 0;
-      }
-      this.setState({ typing: this.titles[this.prompt] });
-    }
-
-    this.setState((state) => ({
-      seconds: state.seconds + 1,
-    }));
-    if (this.state.seconds === 5) {
-      this.setState((state) => ({
-        seconds: 0,
-      }));
-    }
-  }
-
-  componentDidMount() {
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
   }
 
   render() {
     return (
       <div className="home">
-        <div className="spark">
-          <img id="sparkle-1" className="sparkle" src={Sparkle} alt="" />
-          <img id="sparkle-2" className="sparkle" src={Sparkle} alt="" />
-          <img id="sparkle-3" className="sparkle" src={Sparkle} alt="" />
-          <img id="sparkle-4" className="sparkle" src={Sparkle} alt="" />
-
-          <div className="intro">
-            <div className="line">
-              <span className="intro-generic">👋 Hello, my name</span>
-            </div>
-            <div className="line l2">
-              <span className="intro-generic">is</span>
-              <span className="intro-name">Scarlett Kadan</span>
-            </div>
-            <div id="type-1" className="line l3">
-              <span className="intro-generic">I'm a </span>
-              <div className="typing">
-                <span
-                  className="typing-text glitch layers"
-                  data-text={this.state.typing}
-                >
-                  {this.state.typing}
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Intro />
+        <About />
       </div>
     );
   }
