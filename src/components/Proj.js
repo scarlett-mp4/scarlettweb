33e6c6ccf9c6605fc8d619c9420106283bac4223
Scar 3 <kadansk8@gmail.com>
2024-02-10 21:31:51 -0500

    (actually) fixed tiles

--- src/components/Proj.js
+++ src/components/Proj.js
@@ -1,29 +1,30 @@
 import React, { Component } from "react";
+import styled from 'styled-components';
 import "../assets/stylesheets/proj.scss";
 
+// Define ProjLink outside of the Proj class
+const ProjLink = styled.a`
+  &::after {
+    background-image: url("${props => props.n}");
+  }
+  background-image: linear-gradient(270deg, ${props => props.c1}, ${props => props.c2}, ${props => props.c3});
+`;
+
 export default class Proj extends Component {
-  constructor(props) {
-    super(props);
-    this.id = Math.random().toString(36).substring(7);
-    this.style = document.createElement("style");
-    this.style.innerHTML = `
-    #${this.id} {
-      background-image: linear-gradient(270deg, ${this.props.c1}, ${this.props.c2}, ${this.props.c3});
-    }
-    
-    #${this.id}::after {
-      background-image: url("${this.props.n}");
-    }
-  `;
-  }
-
-  componentDidMount() {
-    document.getElementById(this.id).appendChild(this.style);
-  }
-
   render() {
+    // Use ProjLink with props passed from Proj
     return (
-      <a id={this.id} href={this.props.to} target="_blank" rel="noreferrer" className="proj">
+      <ProjLink
+        id={this.props.id}
+        href={this.props.to}
+        target="_blank"
+        rel="noreferrer"
+        className="proj"
+        c1={this.props.c1}
+        c2={this.props.c2}
+        c3={this.props.c3}
+        n={this.props.n}
+      >
         <div className="proj-header">
           <div className="proj-language">
             <img className="proj-language-icon" src={this.props.icon} alt="" />
@@ -33,7 +34,7 @@ export default class Proj extends Component {
         </div>
         <div className="proj-title">{this.props.title}</div>
         <div className="proj-desc">{this.props.desc}</div>
-      </a>
+      </ProjLink>
     );
   }
 }
