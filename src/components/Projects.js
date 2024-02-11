e846fd313224ffc0a3454b231a7ffac9733303ef
Scar 3 <kadansk8@gmail.com>
2024-02-10 19:47:57 -0500

    animations, better scaling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { motion } from "framer-motion";
 import Proj from "./Proj";
 import "../assets/stylesheets/projects.scss";
 
@@ -6,14 +7,17 @@ export default class Projects extends Component {
   render() {
     return (
       <div className="projects">
-        <div className="projects-header">
+        <motion.div
+            initial={{ opacity: 0, rotateX: 90 }}
+            animate={{ opacity: 1, rotateX: 0 }}
+            transition={{ duration: 0.5, delay: 0 }} className="projects-header">
           <span className="projects-title">🎨&nbsp;&nbsp;Projects</span>
           <span className="projects-subtitle">
             Here are some of the projects I've worked on or am currently working
             on.
           </span>
           <div className="projects-divider"></div>
-        </div>
+        </motion.div>
         <div className="projects-content">
           <Proj
             title="kadan.live 💻"
@@ -25,7 +29,7 @@ export default class Projects extends Component {
             c2="#d400f0"
             c3="#00b8f0"
             n="https://media.discordapp.net/attachments/1035798463377588254/1206020619775512636/noise.jpg?ex=65da7d3a&is=65c8083a&hm=b567e4c6589af56c4d25f3d9e046ede0f0d877082750148cd4a628a1e76198ec&=&format=webp"
-            to="https://google.com"
+            to="https://github.com/Skarless/scarlettweb"
           />
         </div>
       </div>
